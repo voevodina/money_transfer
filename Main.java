17309345c87d47a89207f8d53c8a185992af0bc9
voevodina <voevodina_lyubov@mail.ru>
2020-03-28 20:51:44 +0300

    Added files

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int transfer = 500_000_000;
+        int sum = 2_000_000_000 + 500_000_000;
+        System.out.println(sum);
+    }
+}
